d96ff5185b9538b22c1d3537df53ec51701faa26
Brandon Goodin <brandon.goodin@gmail.com>
2010-08-17 13:55:25 -0500

    cleanup

--- assemble.sh
+++
@@ -1,2 +0,0 @@
-mvn clean deploy site-deploy assembly:single
-scp target/stripes-guice-dist* bgoodin@silvermindsoftware.com:/usr/local/resin/hosts/www.silvermindsoftware.com/webapps/ROOT/stripesguice-dist/
--- local-assemble.sh
+++
@@ -1,1 +0,0 @@
-mvn clean install site assembly:single
