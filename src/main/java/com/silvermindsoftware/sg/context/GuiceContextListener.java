2cd9122d96316e84c8ceb6cf9d3c26a8d5712c8e
BRANDON GOODIN <brandon.goodin@gmail.com>
2017-08-04 03:36:13 -0500

    javadoc fix

--- src/main/java/com/silvermindsoftware/sg/context/GuiceContextListener.java
+++ src/main/java/com/silvermindsoftware/sg/context/GuiceContextListener.java
@@ -23,18 +23,18 @@ import static com.silvermindsoftware.sg.utils.Helpers.isEmptyOrNull;
  * comma-delimited list of Modules you want to initialize.
  * </p>
  * <code>
- * &lt;listener&gt;<br/>
- * &nbsp;&nbsp;&lt;listener-class&gt;<br/>
- * &nbsp;&nbsp;&nbsp;&nbsp;com.silvermindsoftware.sg.context.GuiceContextListener<br/>
- * &nbsp;&nbsp;&lt;/listener-class&gt;<br/>
- * &lt;/listener&gt;<br/>
+ * &lt;listener&gt;<br>
+ * &nbsp;&nbsp;&lt;listener-class&gt;<br>
+ * &nbsp;&nbsp;&nbsp;&nbsp;com.silvermindsoftware.sg.context.GuiceContextListener<br>
+ * &nbsp;&nbsp;&lt;/listener-class&gt;<br>
+ * &lt;/listener&gt;<br>
  * </code>
- * <br/>
+ * <br>
  * <code>
- * &lt;context-param&gt;<br/>
- * &nbsp;&nbsp;&lt;param-name&gt;Guice.Modules&lt;/param-name&gt;<br/>
- * &nbsp;&nbsp;&lt;param-value&gt;fully-qualified-module-class-name[[,fully-qualified-module-class-name]... ]&lt;/param-value&gt;<br/>
- * &lt;/context-param&gt;<br/>
+ * &lt;context-param&gt;<br>
+ * &nbsp;&nbsp;&lt;param-name&gt;Guice.Modules&lt;/param-name&gt;<br>
+ * &nbsp;&nbsp;&lt;param-value&gt;fully-qualified-module-class-name[[,fully-qualified-module-class-name]... ]&lt;/param-value&gt;<br>
+ * &lt;/context-param&gt;<br>
  * </code>
  * <p>
  * Alternate functionality can be provided for creating the Injector. Simply
@@ -43,10 +43,10 @@ import static com.silvermindsoftware.sg.utils.Helpers.isEmptyOrNull;
  * DefaultGuiceInjectorFactory} and specify it in an context-param:
  * </p>
  * <code>
- * &lt;context-param&gt;<br/>
- * &nbsp;&nbsp;&lt;param-name&gt;GuiceInjectorFactory.Class&lt;/param-name&gt;<br/>
- * &nbsp;&nbsp;&lt;param-value&gt;fully-qualified-class-name&lt;/param-value&gt;<br/>
- * &lt;/context-param&gt;<br/>
+ * &lt;context-param&gt;<br>
+ * &nbsp;&nbsp;&lt;param-name&gt;GuiceInjectorFactory.Class&lt;/param-name&gt;<br>
+ * &nbsp;&nbsp;&lt;param-value&gt;fully-qualified-class-name&lt;/param-value&gt;<br>
+ * &lt;/context-param&gt;<br>
  * </code>
  */
 public class GuiceContextListener extends GuiceServletContextListener {
